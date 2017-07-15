3f949ae11e0ec0a68e254a26cab65fe8f41382e9
jeffreywescott <jeffrey@learnersguild.org>
2017-07-15 12:31:08 -0700

    break week calculators

---
+++ src/__tests__/breakWeeks.test.js
@@ -0,0 +1,92 @@
+import test from 'tape'
+import moment from 'moment'
+
+import {
+  momentDayOnly,
+  throwsIfInvalidDate,
+} from '../util'
+
+import {
+  summerBreakWeekMonday,
+  summerBreakWeekDays,
+  winterBreakWeekMonday,
+  winterBreakWeekDays,
+} from '../breakWeeks'
+
+test('src/breakWeeks', t => {
+  t.test('summerBreakWeekMonday', tt => {
+    tt.test('throws if the given date is not a date', throwsIfInvalidDate(summerBreakWeekMonday))
+
+    tt.test('returns 2017-06-12 if given date is before that', ttt => {
+      ttt.plan(1)
+      const mon = summerBreakWeekMonday('2017-02-27')
+      const expected = momentDayOnly('2017-06-12')
+      ttt.true(mon.isSame(expected), 'should be 2017-06-12')
+    })
+
+    tt.test('returns Monday of last fully-contained week in June', ttt => {
+      ttt.plan(1)
+      const mon = summerBreakWeekMonday('2020-01-06')
+      const expected = momentDayOnly('2020-06-22')
+      ttt.true(mon.isSame(expected), 'should be the Monday of the last fully-contained week in June')
+    })
+  })
+
+  t.test('summerBreakWeekDays', tt => {
+    tt.test('throws if the given date is not a date', throwsIfInvalidDate(summerBreakWeekDays))
+
+    tt.test('returns [2017-06-12 .. 2017-16-16] if given date is before that', ttt => {
+      ttt.plan(5)
+      const days = summerBreakWeekDays('2017-02-27')
+      const expected = [
+        momentDayOnly('2017-06-12'),
+        momentDayOnly('2017-06-13'),
+        momentDayOnly('2017-06-14'),
+        momentDayOnly('2017-06-15'),
+        momentDayOnly('2017-06-16'),
+      ]
+      days.forEach((day, i) => ttt.true(day.isSame(expected[i]), 'should be the week of 2017-06-12'))
+    })
+
+    tt.test('returns last fully-contained week in June', ttt => {
+      ttt.plan(5)
+      const days = summerBreakWeekDays('2018-01-01')
+      const expected = [
+        momentDayOnly('2018-06-25'),
+        momentDayOnly('2018-06-26'),
+        momentDayOnly('2018-06-27'),
+        momentDayOnly('2018-06-28'),
+        momentDayOnly('2018-06-29'),
+      ]
+      days.forEach((day, i) => ttt.true(day.isSame(expected[i]), 'should be the last fully-contained week in June'))
+    })
+  })
+
+  t.test('winterBreakWeekMonday', tt => {
+    tt.test('throws if the given date is not a date', throwsIfInvalidDate(winterBreakWeekMonday))
+
+    tt.test('returns Monday of last fully-contained week in December', ttt => {
+      ttt.plan(1)
+      const mon = winterBreakWeekMonday('2016-07-11')
+      const expected = momentDayOnly('2016-12-26')
+      ttt.true(mon.isSame(expected), 'should be the Monday of the last fully-contained week in December')
+    })
+  })
+
+  t.test('winterBreakWeekDays', tt => {
+    tt.test('throws if the given date is not a date', throwsIfInvalidDate(winterBreakWeekDays))
+
+    tt.test('returns last fully-contained week in December', ttt => {
+      ttt.plan(5)
+      const days = winterBreakWeekDays('2020-01-06')
+      const expected = [
+        momentDayOnly('2020-12-21'),
+        momentDayOnly('2020-12-22'),
+        momentDayOnly('2020-12-23'),
+        momentDayOnly('2020-12-24'),
+        momentDayOnly('2020-12-25'),
+      ]
+      days.forEach((day, i) => ttt.true(day.isSame(expected[i]), 'should be the last fully-contained week in December'))
+    })
+  })
+})
---
+++ src/breakWeeks.js
@@ -0,0 +1,55 @@
+import moment from 'moment'
+
+import {momentDayOnly} from './util'
+
+const _lastFullWeekOfMonthMonday = (month, date = new Date()) => {
+  const input = momentDayOnly(date)
+  const output = input
+    .clone()
+    .startOf('year')
+    .month(month)
+    .startOf('isoWeek')
+  const firstOfNextMonth = output
+    .clone()
+    .month(month + 1)
+    .startOf('month')
+
+  while (output.clone().add(13, 'days').isSameOrBefore(firstOfNextMonth)) {
+    output.add(1, 'week')
+  }
+
+  // ensure that we get the _next_ one
+  output.add(output > input ? 0 : 52, 'weeks')
+
+  return output
+}
+
+const _weekdaysForMonday = mon => {
+  return [
+    mon,
+    mon.clone().add(1, 'days'),
+    mon.clone().add(2, 'days'),
+    mon.clone().add(3, 'days'),
+    mon.clone().add(4, 'days'),
+  ]
+}
+
+export const summerBreakWeekMonday = date => {
+  const input = momentDayOnly(date)
+  const summer2017BreakWeekMonday = momentDayOnly('2017-06-12')
+  return input.isBefore(summer2017BreakWeekMonday) ?
+    summer2017BreakWeekMonday :
+    _lastFullWeekOfMonthMonday(5, date)
+}
+
+export const winterBreakWeekMonday = date => _lastFullWeekOfMonthMonday(11, date)
+
+export const summerBreakWeekDays = date => {
+  const mon = summerBreakWeekMonday(date)
+  return _weekdaysForMonday(mon)
+}
+
+export const winterBreakWeekDays = date => {
+  const mon = winterBreakWeekMonday(date)
+  return _weekdaysForMonday(mon)
+}
