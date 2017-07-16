977ea16417cb769e3bcbd18b44472c0826608c77
jeffreywescott <jeffrey@learnersguild.org>
2017-07-15 20:56:15 -0700

    improve branding

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Link from 'next/link'
 import Head from 'next/head'
 import withRedux from 'next-redux-wrapper'
 
@@ -12,16 +13,48 @@ import Form from '../components/Form'
 import ImportantDates from '../components/ImportantDates'
 
 const index = () => {
+  const leftIconSrc = 'https://brand.learnersguild.org/assets/learners-guild-icon.svg'
+  const leftIcon = (
+    <Link href="/">
+      <span>
+        <img id="logo" src={leftIconSrc} alt="logo" title="Learners Guild"/>
+        <style jsx>{`
+          img#logo {
+            width: 40px;
+            height: 40px;
+          }
+        `}</style>
+      </span>
+    </Link>
+  )
+  const title = (
+    <span>
+      <h1 className="title">ISA Calculator</h1>
+      <style jsx>{`
+        h1.title {
+          display: inline-block;
+          margin-left: 10px;
+          font-family: Roboto;
+          font-size: 1.3em;
+        }
+      `}</style>
+    </span>
+  )
+
   return (
     <div>
       <Head>
+        <link rel="icon" type="image/png" href="https://brand.learnersguild.org/coast-228x228.png"/>
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
-        <link href='/static/theme.css' rel='stylesheet'/>
+        <link href="/static/theme.css" rel="stylesheet"/>
       </Head>
       <ThemeProvider theme={theme}>
         <div>
-          <AppBar title='ISA Calculator'/>
+          <AppBar
+            leftIcon={leftIcon}
+            title={title}
+            />
           <Form/>
           <Calculations/>
           <ImportantDates/>
