e74b1a593f95a54e1207912f4100165bb18b9ac8
jeffreywescott <jeffrey@learnersguild.org>
2017-07-15 23:47:32 -0700

    collapse consecutive dates of same type into one list item

--- components/ImportantDates.js
+++ components/ImportantDates.js
@@ -22,25 +22,46 @@ const iconForType = type => {
   return 'event'
 }
 
+const DAY_MILLIS = 60 * 60 * 24 * 1000
+
 class ImportantDates extends Component {
-  render() {
+  renderDates() {
     const {importantDates} = this.props
 
-    const listItems = Object.keys(importantDates)
-      .sort()
-      .map((date, i) => {
-        const type = importantDates[date]
-        const caption = formatDate(date)
+    const sortedDates = Object.keys(importantDates).sort()
+
+    return sortedDates
+      .reduce((keys, currDateStr, i) => {
+        const prevDateStr = sortedDates[i - 1]
+        const prevType = i === 0 ? null : importantDates[prevDateStr]
+        const prevDate = i === 0 ? new Date('1970-01-01') : new Date(prevDateStr)
+        const currType = importantDates[currDateStr]
+        const currDate = new Date(currDateStr)
+        const diffMillis = currDate.getTime() - prevDate.getTime()
+        if (diffMillis <= DAY_MILLIS && prevType === currType) {
+          keys[keys.length - 1].end = currDateStr
+        } else {
+          keys.push({start: currDateStr, end: currDateStr, type: currType})
+        }
+        return keys
+      }, [])
+      .map((dateInfo, i) => {
+        const {start, end, type} = dateInfo
+        const caption = start === end ?
+          formatDate(start) :
+          `${formatDate(start)} — ${formatDate(end)}`
         return (
           <ListItem key={i} leftIcon={iconForType(type)} caption={caption}/>
         )
       })
+  }
 
+  render() {
     return (
       <Card style={cardStyle}>
         <CardTitle title="Important Dates"/>
         <List>
-          {listItems}
+          {this.renderDates()}
         </List>
       </Card>
     )
