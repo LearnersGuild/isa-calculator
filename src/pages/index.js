49ccb6a036071e4d1aeab2cad074cc9e04b41197
jeffreywescott <jeffrey@learnersguild.org>
2017-07-18 19:25:09 -0700

    use favicon metadata from brand.learnersguild.org

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,15 +2,24 @@ import React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import withRedux from 'next-redux-wrapper'
+import {Parser as HtmlToReactParser} from 'html-to-react'
 
 import AppBar from 'react-toolbox/lib/app_bar/AppBar'
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider'
 
 import store from '../store'
 import theme from '../static/theme'
+import iconsMetadata from '../static/icons-metadata.json'
 import Calculations from '../components/Calculations'
 import Form from '../components/Form'
 import ImportantDates from '../components/ImportantDates'
+
+// favicons
+const h2r = new HtmlToReactParser()
+const iconsMetadataElements = iconsMetadata.map((link, i) => {
+  const {type, props} = h2r.parse(link)
+  return React.createElement(type, {...props, key: i})
+})
 
 const index = () => {
   const leftIconSrc = 'https://brand.learnersguild.org/assets/learners-guild-icon.svg'
@@ -45,7 +54,7 @@ const index = () => {
     <div>
       <Head>
         <title>ISA Calculator</title>
-        <link rel="icon" type="image/png" href="https://brand.learnersguild.org/coast-228x228.png"/>
+        {iconsMetadataElements}
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet"/>
         <link href="/static/theme.css" rel="stylesheet"/>
